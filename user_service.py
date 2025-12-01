3b92ccd85743cb449106768e7450dae49dcefd57
Om-Borse26 <omborse1618@gmail.com>
2025-12-01 15:10:49 +0530

    new pipeline

--- user_service.py
+++ user_service.py
@@ -25,7 +25,7 @@ DB_HOST = os.getenv('DB_HOST', 'shopease-mysql-db.cmni2wmcozyh.us-east-1.rds.ama
 DB_PORT = os.getenv('DB_PORT', '3306')
 DB_USER = os.getenv('DB_USER', 'admin')
 DB_PASSWORD = os.getenv('DB_PASSWORD', 'ChangeMe123!')
-DB_NAME = os.getenv('DB_NAME', 'shopeas')
+DB_NAME = os.getenv('DB_NAME', 'shopease')
 
 DATABASE_URI = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
